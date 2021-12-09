029155045a7430f77a11f3282ae9aa07925d9cc6
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2021-12-09 10:33:37 +0100

    Horizontal width, we have it

--- day08.py
+++ day08.py
@@ -3,24 +3,14 @@ from itertools import permutations
 from aoc import get_input
 
 
-"""
-  0:      1:      2:      3:      4:
- aaaa    ....    aaaa    aaaa    ....
-b    c  .    c  .    c  .    c  b    c
-b    c  .    c  .    c  .    c  b    c
- ....    ....    dddd    dddd    dddd
-e    f  .    f  e    .  .    f  .    f
-e    f  .    f  e    .  .    f  .    f
- gggg    ....    gggg    gggg    ....
-
-  5:      6:      7:      8:      9:
- aaaa    aaaa    aaaa    aaaa    aaaa
-b    .  b    .  .    c  b    c  b    c
-b    .  b    .  .    c  b    c  b    c
- dddd    dddd    ....    dddd    dddd
-.    f  e    f  .    f  e    f  .    f
-.    f  e    f  .    f  e    f  .    f
- gggg    gggg    ....    gggg    gggg"""
+"""0:     1:      2:      3:      4:      5:      6:      7:      8:      9:
+ aaaa    ....    aaaa    aaaa    ....    aaaa    aaaa    aaaa    aaaa    aaaa
+b    c  .    c  .    c  .    c  b    c  b    .  b    .  .    c  b    c  b    c
+b    c  .    c  .    c  .    c  b    c  b    .  b    .  .    c  b    c  b    c
+ ....    ....    dddd    dddd    dddd    dddd    dddd    ....    dddd    dddd
+e    f  .    f  e    .  .    f  .    f  .    f  e    f  .    f  e    f  .    f
+e    f  .    f  e    .  .    f  .    f  .    f  e    f  .    f  e    f  .    f
+ gggg    ....    gggg    gggg    ....    gggg    gggg    ....    gggg    gggg"""
 DIGITS = {frozenset('abcefg'): 0,
           frozenset('cf'): 1,
           frozenset('acdeg'): 2,
